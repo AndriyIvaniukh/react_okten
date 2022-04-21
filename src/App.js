cb20fb72ffc5b26da33c46a791c882722e3b79f8
AndriyIvaniukh <andriyivaniukh@gmail.com>
2022-04-21 19:33:24 +0300

    react lesson5 part2 lection parts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useReducer} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const init = (initCount) => {
+    return {
+      counter1: initCount,
+      counter2: initCount
+    }
 }
 
-export default App;
+const reducer = (state, action) =>{
+  switch (action.type){
+    case 'inc':
+      return {...state, counter1: state.counter1++};
+    case 'dec':
+      return {...state, counter1: state.counter1--};
+    case 'reset':
+      return {...state, counter1: action.payload};
+  }
+
+}
+
+const App = () => {
+
+    const [state,dispatch] = useReducer(reducer, 0, init);
+
+    return (
+        <div>
+          <div>
+            {state.counter1}
+            <button onClick={()=>dispatch({type: 'inc'})}>inc</button>
+            <button onClick={()=>dispatch({type: 'dec'})}>dec</button>
+            <button onClick={()=>dispatch({type: 'reset', payload: 0})}>reset</button>
+          </div>
+        </div>
+    );
+};
+
+export {App};
